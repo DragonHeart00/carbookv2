46f28e322cf66c1408870e04216c3ed015c6c678
sulaimankasas <ska@easypartz.com>
2023-11-06 08:59:35 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import WelcomeScreen from './src/screens/WelcomScreen';
+
+function App(): JSX.Element {
+
+  return (
+    <WelcomeScreen/>
+  );
+}
+
+export default App;
