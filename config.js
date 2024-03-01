2b2a78efa7137fb31808cd339f91c7e10cfa4987
DragonHeart <sulaiman96@outlook.dk>
2024-03-01 17:37:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,7 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDxekVZESj3TMjvSvHGrwiPhvU2s-UbP_s",
-  authDomain: "autoservice-fb74d.firebaseapp.com",
-  projectId: "autoservice-fb74d",
-  storageBucket: "autoservice-fb74d.appspot.com",
-  messagingSenderId: "618670213485",
-  appId: "1:618670213485:web:a2e2ff08a2fbe2dae73264",
-  measurementId: "G-BRGK4YWFXE"
-};
+
 
 // Initialize Firebase
 if (!firebase.apps.length) {
